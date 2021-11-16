fe8c5703123b1e6e3b1556871206bb2369392fb2
williamvega1393 <william.vega@uptc.edu.co>
2021-11-16 12:02:52 -0500

    config array list

--- src/com/company/controllers/PersonController.java
+++ src/com/company/controllers/PersonController.java
@@ -2,6 +2,9 @@ package com.company.controllers;
 
 import com.company.models.Person;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
 
 public class PersonController {
     public String personToJson(Person person) {
@@ -12,5 +15,16 @@ public class PersonController {
     public Person jsonToPerson(String json) {
         Gson gson = new Gson();
         return gson.fromJson(json, Person.class);
+    }
+
+    public String personsToJson(ArrayList<Person> person) {
+        Gson gson = new Gson();
+        return gson.toJson(person);
+    }
+
+    public ArrayList<Person> jsonToPersons(String json) {
+        Gson gson = new Gson();
+        return gson.fromJson(json, new TypeToken<ArrayList<Person>>() {
+        }.getType());
     }
 }
